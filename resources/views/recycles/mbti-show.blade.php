ad1349a93a654a444d7318fb12c30482c8c61ea2
sungjun <zkfmak10@gmail.com>
2021-04-19 16:27:01 +0900

    subview edit

--- resources/views/recycles/mbti-show.blade.php
+++ resources/views/recycles/mbti-show.blade.php
@@ -12,8 +12,8 @@
     @auth()
         @if($mbti->user_id == auth()->user()->id)
             <div class="w-10/12 mx-auto my-4 text-lg">
-                <span class="hover:text-blue-300"><a href="{{route('mbtis.enfj.edit', $mbti->id)}}"><i class="xi-pen-o pr-2"></i><button>수정</button></a></span>
-                <form action="{{route('mbtis.enfj.destroy', $mbti->id)}}" method="post" class="inline-block">
+                <span class="hover:text-blue-300"><a href="{{route('mbtis.'.$mbti->mbtiSort.'.edit', $mbti->id)}}"><i class="xi-pen-o pr-2"></i><button>수정</button></a></span>
+                <form action="{{route('mbtis.'.$mbti->mbtiSort.'.destroy', $mbti->id)}}" method="post" class="inline-block">
                     @csrf
                     @method('delete')
                     <span class="hover:text-red-300"><i class="xi-cut pr-2 pl-4"></i>
