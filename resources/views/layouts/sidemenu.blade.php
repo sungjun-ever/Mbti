56e2bc518a45b671f9013f21c044fa447c150648
sungjun <zkfmak10@gmail.com>
2021-04-21 17:04:00 +0900

    sideMenu view

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,8 +1,21 @@
 <div id="mySidenav"
-     class="flex flex-column h-full w-2/12 position-fixed z-0 top-0 left-0
-             overflow-x-hidden bg-gray-50 hidden">
+     class="flex flex-column h-full w-5/12 xl:w-2/12 position-fixed z-0 top-0 left-0
+             overflow-x-hidden bg-gray-50 hidden shadow-lg px-1">
     <div class="h-16 text-right py-3 pr-3">
         <button class="hover:text-blue-300 text-xl" onclick="closeNav()">X</button>
+    </div>
+    <div class="my-4 py-3 bg-blue-300 text-base text-white text-center flex justify-around rounded">
+        @guest
+            <a href="{{route('loginPage')}}" class="mr-2"><button class="hover:text-blue-500">로그인</button></a>
+            <a href="{{route('registerPage')}}"><button class="hover:text-blue-500">회원가입</button></a>
+        @endguest
+        @auth()
+            <a href="#" class="mr-2"><button class="hover:hover:text-blue-500">{{auth()->user()->name}}</button></a>
+            <form action="{{route('logout')}}" method="post" class="inline-block">
+                @csrf
+                <button class="hover:text-blue-500">로그아웃</button>
+            </form>
+        @endauth
     </div>
     <div>
         <div id="openSubBtn" class="bg-blue-300 py-2 pl-3 text-white rounded" onclick="openSubNav()">
