50c26c0574830d9667a372c093b7d7a591836257
sungjun <zkfmak10@gmail.com>
2021-04-21 16:24:04 +0900

    sidemenu view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,96 +11,7 @@
 </head>
 <body class="bg-gray-100 h-screen">
 <header class="flex bg-blue-300 h-16 align-items-center shadow-md">
-    <div id="mySidenav"
-         class="flex flex-column h-full w-2/12 position-fixed z-0 top-0 left-0
-             overflow-x-hidden bg-gray-50 hidden">
-        <div class="h-16 text-right py-3 pr-3">
-            <button class="hover:text-blue-300 text-xl" onclick="closeNav()">X</button>
-        </div>
-        <div>
-            <div id="openSubBtn" class="bg-blue-300 py-2 pl-3 text-white rounded" onclick="openSubNav()">
-                <button class="text-md text-center hover:text-blue-500">성격유형</button>
-            </div>
-            <div id="closeSubBtn" class="bg-blue-300 py-2 pl-3 text-white rounded hidden" onclick="closeSubNav()">
-                <button class="text-md text-center hover:text-blue-500">성격유형</button>
-            </div>
-            <div id="mbtiNav" class="py-2 hidden pl-2 bg-white shadow-md">
-                <a href="{{route('mbtis.enfj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ENFJ
-                </a>
-
-                <a href="{{route('mbtis.enfp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ENFP
-                </a>
-
-                <a href="{{route('mbtis.entj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ENTJ
-                </a>
-
-                <a href="{{route('mbtis.entp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ENTP
-                </a>
-
-                <a href="{{route('mbtis.esfj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ESFJ
-                </a>
-
-                <a href="{{route('mbtis.esfp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ESFP
-                </a>
-
-                <a href="{{route('mbtis.estj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ESTJ
-                </a>
-
-                <a href="{{route('mbtis.estp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ESTP
-                </a>
-
-                <a href="{{route('mbtis.infj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    INFJ
-                </a>
-
-                <a href="{{route('mbtis.infp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    INFP
-                </a>
-
-                <a href="{{route('mbtis.intj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    INTJ
-                </a>
-
-                <a href="{{route('mbtis.intp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    INTP
-                </a>
-
-                <a href="{{route('mbtis.isfj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ISFJ
-                </a>
-
-                <a href="{{route('mbtis.isfp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ISFP
-                </a>
-
-                <a href="{{route('mbtis.istj.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ISTJ
-                </a>
-
-                <a href="{{route('mbtis.istp.index')}}" class="hover:text-blue-300 py-2 xl:inline-block block">
-                    ISTP
-                </a>
-            </div>
-            <div class="bg-blue-300 py-2 mt-1 text-white rounded">
-                <a href="{{route('frees.index')}}" class="inline-block pl-3">
-                    <button class="hover:text-blue-500">자유게시판</button>
-                </a>
-            </div>
-            <div class="bg-blue-300 py-2 mt-1 text-white rounded">
-                <a href="{{route('suggests.index')}}" class="inline-block pl-3">
-                    <button class="hover:text-blue-500">건의게시판</button>
-                </a>
-            </div>
-        </div>
-    </div>
+    @include('layouts.sidemenu')
     <div class="w-3/12 text-2xl">
         <i class="xi-bars pl-4 cursor-pointer hover:text-gray-50" onclick="openNav()"></i>
     </div>
@@ -137,8 +48,7 @@
     @show
 </section>
 <footer class="w-full xl:w-7/12 min-h-30 mx-auto pt-8">
-    Copyright<br>
-
+    Copyright
 </footer>
 <script>
     function openNav() {
