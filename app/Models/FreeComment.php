772d17aef8f732a4a2c64f75b0fb348be1e8cf8b
sungjun <zkfmak10@gmail.com>
2021-04-29 16:25:40 +0900

    free comments

---
+++ app/Models/FreeComment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FreeComment extends Model
+{
+    use HasFactory;
+    protected $fillable = ['story'];
+
+    public function user(){
+        return $this->belongsTo('App\Model\User');
+    }
+
+    public function free()
+    {
+        return $this->belongsTo('App\Model\Free');
+    }
+}
