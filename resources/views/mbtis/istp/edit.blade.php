ec83f2c3a650cee3a5e5bfb916413e4c8e45a006
sungjun <zkfmak10@gmail.com>
2021-04-30 13:57:52 +0900

    edit view

--- resources/views/mbtis/istp/edit.blade.php
+++ resources/views/mbtis/istp/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('container')
-    @include('recycles.edit', ['mbti'=>$mbti])
+    @include('recycles.edit', ['post'=>$mbti])
 @stop
--- resources/views/recycles/edit.blade.php
+++ resources/views/recycles/edit.blade.php
@@ -1,18 +1,18 @@
 <div class="w-11/12 pt-6 mx-auto">
     <div>
-        <form action="{{route($mbti->board_name.'.update', $mbti->id)}}" method="post">
+        <form action="{{route($post->board_name.'.update', $post->id)}}" method="post">
             @method('PUT')
             @csrf
             <label for="title"></label>
             <input id="title" type="text" name="title"
                    class="w-full py-3 pl-2 text-lg rounded-md outline-none border-2 focus:border-blue-300 truncate
                            @error('title') border-2 border-red-600 @enderror"
-                   value="{{old('title') ? old('title') : $mbti->title}}">
+                   value="{{old('title') ? old('title') : $post->title}}">
             <label for="story"></label>
             <textarea id="story" name="story"
                       class="px-2 pt-2 border-2 focus:border-blue-300 rounded-md outline-none w-full resize-none text-lg
                             @error('story') border-2 border-red-600 @enderror"
-                      rows="24">{{old('story') ? old('story') : $mbti->story}}</textarea>
+                      rows="24">{{old('story') ? old('story') : $post->story}}</textarea>
             <div class="mt-4 text-center">
                 <button type="submit" class="px-4 py-2 mr-4 text-lg rounded-lg text-gray-50 bg-blue-400 hover:bg-blue-800">수정</button>
                 <button class="px-4 py-2 text-lg rounded-lg text-gray-50 bg-red-400 hover:bg-red-800">취소</button>
