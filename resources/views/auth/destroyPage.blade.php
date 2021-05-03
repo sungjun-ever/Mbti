c4463f57beaa272284a6a263e53174b321321709
sungjun <zkfmak10@gmail.com>
2021-05-03 20:09:38 +0900

    user destroy

--- resources/views/auth/destroyPage.blade.php
+++ resources/views/auth/destroyPage.blade.php
@@ -8,7 +8,7 @@
             <div class="pt-8 pb-4 px-1 shadow-md text-center">
                 <span class="text-lg">계정을 삭제하시겠습니까?</span>
                 <div class="mt-4">
-                    <button type="submit" class="bg-red-500 hover:bg-red-800 px-3 py-1 mr-4 text-gray-50 rounded-lg" onclick="confirm('삭제하시겠습니까?')">삭제</button>
+                    <button type="submit" class="bg-red-500 hover:bg-red-800 px-3 py-1 mr-4 text-gray-50 rounded-lg" onclick="if(!confirm('삭제하시겠습니까?')) return false">삭제</button>
                     <a href="{{route('home')}}"><button type="button" class="bg-green-500 hover:bg-green-800 px-3 py-1 text-gray-50 rounded-lg">취소</button></a>
                 </div>
             </div>
