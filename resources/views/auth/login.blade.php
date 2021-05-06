1ce41b5bd9b164532ef62df5648427e2c2fa4097
sungjun <zkfmak10@gmail.com>
2021-05-06 13:20:11 +0900

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,33 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+@section('container')
+    <form action="{{route('login')}}" method="post" class="w-1/2 mx-auto pt-24">
+        @csrf
+        <div class="pt-8 pb-4 px-1 shadow-md">
+            <div>
+                <label for="email" class="text-lg">이메일</label>
+                <input id="email" type="email" name="email" autofocus
+                       value="{{old('email') ? old('email') : ''}}"
+                       class="w-full py-2 shadow-sm rounded-sm pl-2 text-lg @error('email') border-2 border-red-600 @enderror">
+            </div>
+            <div class="mt-4">
+                <label for="password" class="text-lg">비밀번호</label>
+                <input id="password" type="password" name="password"
+                       class="w-full py-2 shadow-sm rounded-sm pl-2 text-lg @error('password') border-2 border-red-600 @enderror">
+            </div>
+            <div class="mt-4">
+                <label for="remember"></label>
+                <input id="remember" type="checkbox" name="remember_me" class="w-5 h-5">
+                <span class="inline-block text-lg h-8">로그인 유지</span>
+                <div class="h-8 mt-2 text-right">
+                    <a href="{{route('findPwPage')}}"><button type="button" class="text-base text-blue-300 hover:text-blue-500">비밀번호 찾기</button></a>
                 </div>
             </div>
+            <div class="mt-8 text-center">
+                <button type="submit"
+                        class="px-3 py-1 mr-4 bg-blue-500 hover:bg-blue-800 text-gray-50 text-lg rounded-lg ">로그인</button>
+                <button class="px-3 py-1 bg-red-500 hover:bg-red-800 text-gray-50 text-lg rounded-lg">취소</button>
+            </div>
         </div>
-    </div>
-</div>
-@endsection
+    </form>
+@stop
