864110ecb7e5c41380734f93b0fbde3d2510b458
sungjun <zkfmak10@gmail.com>
2021-05-07 13:10:46 +0900

    auth

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
+@section('container')
+<div class="pt-16">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
