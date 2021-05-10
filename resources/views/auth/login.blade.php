576ab1c2bd6d5d284c45a6d9d930301e2fe0d660
sungjun <zkfmak10@gmail.com>
2021-05-10 22:22:02 +0900

    passwords view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,7 +20,7 @@
                 <input id="remember" type="checkbox" name="remember_me" class="w-5 h-5">
                 <span class="inline-block text-lg h-8">로그인 유지</span>
                 <div class="h-8 mt-2 text-right">
-                    <a href="{{route('findPwPage')}}"><button type="button" class="text-base text-blue-300 hover:text-blue-500">비밀번호 찾기</button></a>
+                    <a href="{{route('password.request')}}"><button type="button" class="text-base text-blue-300 hover:text-blue-500">비밀번호 찾기</button></a>
                 </div>
             </div>
             <div class="mt-8 text-center">
--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('container')
-<div class="container">
+<div class="container pt-16">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+                <div class="card-header">{{ __('비밀번호 확인') }}</div>
 
                 <div class="card-body">
-                    {{ __('Please confirm your password before continuing.') }}
+                    {{ __('계속하려면 비밀번호를 입력해주세요.') }}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('비밀번호') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -30,12 +30,12 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Confirm Password') }}
+                                    {{ __('비빌번호 확인') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('비밀번호를 잊으셨나요?') }}
                                     </a>
                                 @endif
                             </div>
