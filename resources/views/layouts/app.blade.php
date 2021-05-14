fcadf53687ef799473c64f20022f78cd6a6d3d4a
sungjun <zkfmak10@gmail.com>
2021-05-14 15:06:20 +0900

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,37 +10,37 @@
     <title>@yield('title', 'MBTI')</title>
 </head>
 <body class="bg-gray-100 h-screen">
-<header class="flex bg-blue-300 h-16 align-items-center shadow-md">
-    @include('layouts.sidemenu')
-    <div class="flex-1 text-2xl">
-        <i class="xi-bars pl-4 cursor-pointer hover:text-gray-50" onclick="openNav()"></i>
-    </div>
-    <div class="flex-initial flex pr-4">
-        <div class="flex-initial">
-            <a href="{{route('home')}}">
-                <button class="hover:text-gray-50 xl:text-xl text-lg">MBTI</button>
-            </a>
+<header class="relative bg-blue-300 shadow-md">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6">
+        <div class="flex justify-between items-center py-6 md:justify-start md:space-x-10">
+            @include('layouts.sidemenu')
+            <div class="md:w-2/12 md:flex-initial w-0 flex-1 text-2xl">
+                <i class="xi-bars cursor-pointer hover:text-gray-50" onclick="openNav()"></i>
+            </div>
+            <div class="md:pr-0 pr-4">
+                <a href="{{route('home')}}">
+                    <button class="hover:text-gray-50 text-2xl">MBTI</button>
+                </a>
+            </div>
+            <nav class="hidden md:block md:flex-1 space-x-3 text-base">
+                <a href="{{route('mbtis.index')}}"><button class="hover:text-gray-50">성격유형</button></a>
+                <a href="{{route('frees.index')}}"><button class="hover:text-gray-50">자유게시판</button></a>
+                <a href="{{route('suggests.index')}}" ><button class="hover:text-gray-50">건의게시판</button></a>
+            </nav>
+            <div class="flex-initial space-x-4 text-base text-right">
+                @guest
+                    <a href="{{route('login')}}"><i class="xi-profile-o text-3xl pt-1"></i></a>
+                @endguest
+                @auth()
+                    <a href="{{route('info', auth()->user()->id)}}"><i class="xi-profile text-3xl pt-1"></i></a>
+{{--                    <form action="{{route('logout')}}" method="post" class="inline-block">--}}
+{{--                        @csrf--}}
+{{--                        <button class="hover:text-gray-50">로그아웃</button>--}}
+{{--                    </form>--}}
+                @endauth
+            </div>
         </div>
-        <div class="flex-initial ml-4 xl:text-md text-base pt-1 md:block hidden">
-            <a href="{{route('mbtis.index')}}"><button class="hover:text-gray-50">성격유형</button></a>
-            <a href="{{route('frees.index')}}"><button class="hover:text-gray-50 ml-3">자유게시판</button></a>
-            <a href="{{route('suggests.index')}}" ><button class="hover:text-gray-50 ml-3">건의게시판</button></a>
-        </div>
     </div>
-    <div class="flex-1 text-right xl:text-lg text-md hidden md:block">
-        @guest
-            <a href="{{route('login')}}" class="mr-2"><button class="hover:text-gray-50">로그인</button></a>
-            <a href="{{route('register')}}"><button class="hover:text-gray-50">회원가입</button></a>
-        @endguest
-        @auth()
-            <a href="{{route('info', auth()->user()->id)}}" class="mr-2"><button class="hover:text-gray-50">{{auth()->user()->name}}</button></a>
-            <form action="{{route('logout')}}" method="post" class="inline-block">
-                @csrf
-                <button class="hover:text-gray-50">로그아웃</button>
-            </form>
-        @endauth
-    </div>
-    <div class="flex-1 hidden xl:block"></div>
 </header>
 
 <section class="bg-white w-full min-h-full mx-auto shadow-md pb-16 xl:w-7/12">
