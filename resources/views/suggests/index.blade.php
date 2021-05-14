c6cab6bfcfa68f299d91827aacb6c7094fe1a4e0
sungjun <zkfmak10@gmail.com>
2021-05-14 15:19:15 +0900

    layout

--- resources/views/suggests/index.blade.php
+++ resources/views/suggests/index.blade.php
@@ -19,14 +19,14 @@
                 </tr>
                 @foreach($sugs as $sug)
                     <tr class="border-b">
-                        <td class="pl-2 truncate">
+                        <td class="pl-2 truncate py-1 text-base">
                             @if($sug->secret === 1)
                                 <a href="{{route($sug->board_name.'.show', $sug->id)}}"
-                                   class="text-lg hover:no-underline">
+                                   class="hover:no-underline">
                                     <i class="xi-lock"></i><span class="text-gray-400 pl-1">비밀글입니다.</span>
                                 </a>
                             @else
-                                <a href="{{route($sug->board_name.'.show', $sug->id)}}" class="text-lg hover:no-underline">
+                                <a href="{{route($sug->board_name.'.show', $sug->id)}}" class="hover:no-underline">
                                     {{$sug->title}}
                                 </a>
                             @endif
