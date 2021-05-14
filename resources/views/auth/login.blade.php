14511039526d90acb3ae1c38bbfa1c044ffeeaf7
sungjun <zkfmak10@gmail.com>
2021-05-14 15:16:24 +0900

    layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('container')
-    <form action="/login" method="post" class="w-1/2 mx-auto pt-24">
+    <form action="{{route('login')}}" method="post" class="lg:w-2/3 w-full mx-auto pt-24">
         @csrf
-        <div class="pt-8 pb-4 px-1 shadow-md">
+        <div class="py-4 lg:px-2 shadow-md">
             <div>
                 <label for="email" class="text-lg">이메일</label>
                 <input id="email" type="email" name="email" autofocus
@@ -19,13 +19,18 @@
                 <label for="remember"></label>
                 <input id="remember" type="checkbox" name="remember_me" class="w-5 h-5">
                 <span class="inline-block text-lg h-8">로그인 유지</span>
-                <div class="h-8 mt-2 text-right">
-                    <a href="{{route('password.request')}}"><button type="button" class="text-base text-blue-300 hover:text-blue-500">비밀번호 찾기</button></a>
+                <div class="h-8 mt-2 text-right space-x-4">
+                    <a href="{{route('register')}}">
+                        <button type="button" class="text-base text-blue-300 hover:text-blue-500">회원가입</button></a>
+                    <a href="{{route('password.request')}}">
+                        <button type="button" class="text-base text-blue-300 hover:text-blue-500">비밀번호 찾기</button>
+                    </a>
                 </div>
             </div>
-            <div class="mt-8 text-center">
+            <div class="mt-8 text-center space-x-4">
                 <button type="submit"
-                        class="px-3 py-1 mr-4 bg-blue-500 hover:bg-blue-800 text-gray-50 text-lg rounded-lg ">로그인</button>
+                        class="px-3 py-1 bg-blue-500 hover:bg-blue-800 text-gray-50 text-lg rounded-lg ">로그인
+                </button>
                 <button class="px-3 py-1 bg-red-500 hover:bg-red-800 text-gray-50 text-lg rounded-lg">취소</button>
             </div>
         </div>
