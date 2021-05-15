6c7f494bc45e089f3755c6269baee2f354cd7a33
sungjun <zkfmak10@gmail.com>
2021-05-15 18:36:50 +0900

    ckeditor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+{{--    <script src="https://cdn.ckeditor.com/ckeditor5/27.1.0/classic/ckeditor.js"></script>--}}
+    <script src="{{asset('js/ckeditor.js')}}"></script>
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .ck-editor__editable{min-height: 700px}
+    </style>
     <title>@yield('title', 'MBTI')</title>
 </head>
 <body class="bg-gray-100 h-screen">
--- resources/views/recycles/create.blade.php
+++ resources/views/recycles/create.blade.php
@@ -8,15 +8,16 @@
             @endif
             <label for="title"></label>
             <input id="title" type="text" name="title"
-                   class="w-full py-3 pl-2 text-lg rounded-md outline-none border-2 focus:border-blue-300
+                   class="w-full py-2 pl-2 text-lg rounded-md outline-none border-2 focus:border-blue-300
                             @error('title') border-2 border-red-600 @enderror"
                    value="{{old('title') ? old('title') : ''}}"
                    placeholder="제목을 입력해주세요.">
-            <label for="story"></label>
-            <textarea id="story" name="story"
-                      class="px-2 pt-2 border-2 focus:border-blue-300 rounded-md outline-none w-full resize-none text-lg
-                             @error('story') border-2 border-red-600 @enderror"
-                      rows="24" placeholder="내용을 입력해주세요.">{{old('story') ? old('story') : ''}}</textarea>
+            <label for="editor"></label>
+            <textarea name="story" id="editor"></textarea>
+{{--            <textarea id="story" name="story"--}}
+{{--                      class="px-2 pt-2 border-2 focus:border-blue-300 rounded-md outline-none w-full resize-none text-lg--}}
+{{--                             @error('story') border-2 border-red-600 @enderror"--}}
+{{--                      rows="24" placeholder="내용을 입력해주세요.">{{old('story') ? old('story') : ''}}</textarea>--}}
             <div class="mt-4 text-center">
                 <button type="submit" class="px-4 py-2 mr-4 text-lg rounded-lg text-gray-50 bg-blue-400 hover:bg-blue-800">작성</button>
                 <button class="px-4 py-2 text-lg rounded-lg text-gray-50 bg-red-400 hover:bg-red-800">취소</button>
@@ -24,3 +25,55 @@
         </form>
     </div>
 </div>
+<script>
+    ClassicEditor
+        .create( document.querySelector( '#editor' ), {
+            toolbar: {
+                items: [
+                    'heading',
+                    '|',
+                    'bold',
+                    'alignment',
+                    'fontColor',
+                    'fontSize',
+                    'fontFamily',
+                    'horizontalLine',
+                    'underline',
+                    'bulletedList',
+                    'numberedList',
+                    '|',
+                    'imageUpload',
+                    'insertTable',
+                    'undo',
+                    'redo'
+                ]
+            },
+            language: 'ko',
+            image: {
+                toolbar: [
+                    'imageTextAlternative',
+                    'imageStyle:full',
+                    'imageStyle:side'
+                ]
+            },
+            table: {
+                contentToolbar: [
+                    'tableColumn',
+                    'tableRow',
+                    'mergeTableCells'
+                ]
+            },
+            simpleUpload: {
+                // The URL that the images are uploaded to.
+                uploadUrl: '/uploadFile',
+
+                // Enable the XMLHttpRequest.withCredentials property.
+                withCredentials: true,
+
+                // Headers sent along with the XMLHttpRequest to the upload server.
+                headers: {
+                    'X-CSRF-TOKEN': 'CSRF-Token',
+                }
+            }
+        } )
+</script>
