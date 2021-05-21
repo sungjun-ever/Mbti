1d0f913a333a3d85cbbb22b63d8db7a3454eb744
sungjun <zkfmak10@gmail.com>
2021-05-21 15:06:17 +0900

    password validation rule

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Rules\IsValidPassword;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -53,7 +54,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed', 'regex:/[a-z]', 'regex:/[0-9]/' , 'regex:/[@$!%*#?&]/'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', new IsValidPassword()],
         ]);
     }
 
