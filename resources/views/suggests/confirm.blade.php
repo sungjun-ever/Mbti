4e67bf7c787e7f52d121c4e24393401d5707166d
sungjun <zkfmak10@gmail.com>
2021-05-21 14:16:08 +0900

    suggest confirm

--- resources/views/suggests/confirm.blade.php
+++ resources/views/suggests/confirm.blade.php
@@ -29,7 +29,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('비빌번호 확인') }}
+                                        {{ __('비밀번호 확인') }}
                                     </button>
                                 </div>
                             </div>
