f4bfe01b3efded32de4ee4798e040542877daa19
sungjun <zkfmak10@gmail.com>
2021-05-24 14:34:29 +0900

    validation lang

--- resources/lang/ko/validation.php
+++ resources/lang/ko/validation.php
@@ -1,4 +1,7 @@
 <?php
 return [
     'required' => ':attribute은 필수로 입력해야합니다.',
+    'confirmed' => ':attribute 비밀번호가 일치하지 않습니다.',
+    'password' => '비밀번호가 일치하지 않습니다.',
+    'unique' => ':attribute이 이미 존재합니다.',
 ];
