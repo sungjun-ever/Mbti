313c6a871acee62bdf0d9bbc3fd659c4b5acf2de
sungjun <zkfmak10@gmail.com>
2021-06-01 13:57:29 +0900

    layout

--- resources/views/auth/user-info.blade.php
+++ resources/views/auth/user-info.blade.php
@@ -4,12 +4,12 @@
     <div class="flex pt-8 mx-auto min-h-full">
         <div class="w-full">
             <div class="flex flex-row text-base pt-8 px-8 pb-4">
-                <a href="{{route('info', auth()->user()->id)}}" class="mr-2"><button class="hover:text-blue-300
-                bg-blue-200 px-1 py-1 rounded-md">회원정보</button></a>
-                <a href="{{route('user.post', auth()->user()->id)}}" class="mr-2"><button class="hover:text-blue-300
-                bg-blue-200 px-1 py-1 rounded-md">작성 게시글</button></a>
-                <a href="{{route('user.comment', auth()->user()->id)}}"><button class="hover:text-blue-300
-                bg-blue-200 px-1 py-1 rounded-md">작성 댓글</button></a>
+                <a href="{{route('info', auth()->user()->id)}}" class="mr-2"><button class="hover:text-green-800
+                bg-green-my text-gray-50 px-1 py-1 rounded-md">회원정보</button></a>
+                <a href="{{route('user.post', auth()->user()->id)}}" class="mr-2"><button class="hover:text-green-800
+                bg-green-my text-gray-50 px-1 py-1 rounded-md">작성 게시글</button></a>
+                <a href="{{route('user.comment', auth()->user()->id)}}"><button class="hover:text-green-800
+                bg-green-my text-gray-50 px-1 py-1 rounded-md">작성 댓글</button></a>
             </div>
 
             <div class="border">
@@ -20,7 +20,7 @@
                         <div class="pt-2">이메일 : {{auth()->user()->email}}</div>
                         <div class="pt-2">가입날짜 : {{auth()->user()->created_at->format('Y-m-d')}}</div>
                     </div>
-                    <div class="mt-4">
+                    <div class="flex mt-4 space-x-4">
                         <a href="{{route('password.reset', auth()->user()->id)}}">
                             <button class="bg-green-500 hover:bg-green-800 px-1 py-1 text-gray-50 rounded-lg">비밀번호 변경</button></a>
                         <a href="{{route('user.destroy', auth()->user()->id)}}">
