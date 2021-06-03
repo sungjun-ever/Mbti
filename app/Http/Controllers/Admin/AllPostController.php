1737f8ca2bfb6c9eabb77dd60e1f85ad86ae3135
sungjun <zkfmak10@gmail.com>
2021-06-03 17:15:21 +0900

    all posts

--- app/Http/Controllers/Admin/AllPostController.php
+++ app/Http/Controllers/Admin/AllPostController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Free;
+use App\Models\Mbti;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AllPostController extends Controller
 {
     public function index()
     {
-        return view('admin.all-post');
+        $mbtiGroup = ['enfj', 'enfp', 'entj', 'entp', 'estj', 'estp', 'esfj', 'esfp',
+            'infj', 'infp', 'intj', 'intp', 'isfj', 'isfp', 'istj', 'istp'];
+        $mbtis = DB::table('mbtis');
+        $all = DB::table('frees')->unionAll($mbtis)->orderByDesc('created_at')->paginate(20, ['*'], 'posts');
+
+        return view('admin.all-post', compact(['all', 'mbtiGroup']));
     }
 }
--- resources/views/admin/all-post.blade.php
+++ resources/views/admin/all-post.blade.php
@@ -7,13 +7,44 @@
             <form action="#" method="get" class="inline-block">
                 @csrf
                 <select name="content" class="border-2 border-green-my focus:outline-none">
-                    <option value="name">이름</option>
                     <option value="title">제목</option>
-                    <option value="story">내용</option>
+                    <option value="group">분류</option>
                 </select>
                 <input type="search" name="search" class="border-2 border-green-my focus:outline-none rounded-md pl-1">
                 <button><i class="xi-search text-lg hover:text-green-800"></i></button>
             </form>
         </div>
+        <table class="w-full table-fixed">
+            <tr class="border-b-2 border-t-2 text-center">
+                <td class="w-1/12">번호</td>
+                <td class="w-2/12">분류</td>
+                <td class="w-3/12">제목</td>
+                <td class="w-2/12">작성 날짜</td>
+                <td class="w-1/12">이동</td>
+            </tr>
+            @foreach($all as $post)
+                <tr class="text-center h-10">
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->board_name}}</td>
+                    <td>
+                        @if(in_array($post->board_name, $mbtiGroup))
+                            <a href={{'../mbti/'.$post->board_name.'/'.$post->id}}>{{$post->title}}</a>
+                        @else
+                            <a href={{'../'.$post->board_name.'/'.$post->id}}>{{$post->title}}</a>
+                        @endif
+                    </td>
+                    <td>{{(new \Carbon\Carbon($post->created_at))->format('Y-m-d')}}</td>
+                    <td>
+                        <form action="#" method="post">
+                            @csrf
+                            <button type="submit" onclick="if(!confirm('임시게시판으로 이동시키겠습니까?')) return false">
+                                <i class="xi-check text-green-400 hover:text-red-500 text-xl"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+        <div class="mt-8">{{$all->links()}}</div>
     </div>
 @stop
