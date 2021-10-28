ecdb26fb3d9db8223956648d32ae16d1acd04f35
sungjun <zkfmak10@gmail.com>
2021-10-28 21:25:42 +0900

    getBoardName trait

--- app/Http/Controllers/TempController.php
+++ app/Http/Controllers/TempController.php
@@ -2,21 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Func\GetBoardName;
 use App\Models\Temp;
 use Illuminate\Http\Request;
 
 class TempController extends Controller
 {
-    public function getBoardName()
-    {
-        $getBoard= explode('/', $_SERVER['REQUEST_URI']);
-        $name = preg_replace('/\?[a-z=&A-Z0-9]*/', '', $getBoard[1]);
-        return $name;
-    }
-
+    use GetBoardName;
     public function index()
     {
-        $boardName = $this->getBoardName();
+        $boardName = $this->boardName();
         $temps = Temp::orderByDesc('id')->paginate(5);
         return view('temp.index', compact(['temps', 'boardName']));
     }
